bcfc8aa0c6d4a2a73e584a176665db022d29124e
N Yashwanth <yashsai8629@gmail.com>
2024-10-24 13:59:37 +0530

    first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String first,last;
+	@JsonIgnore
+	@ManyToMany(cascade = CascadeType.ALL, mappedBy = "employee")
+	private Set<Project> project;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public Set<Project> getProject() {
+		return project;
+	}
+	public void setProject(Set<Project> project) {
+		this.project = project;
+	}
+	
+}
